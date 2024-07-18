5b730063d203fdefbf82c641318a6a317df1a056
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-18 12:35:51 -0400

    made changes to flip coin game

--- src/main/java/com/github/zipcodewilmington/casino/games/coinflip/FlipCoinGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/coinflip/FlipCoinGame.java
@@ -3,9 +3,14 @@ package com.github.zipcodewilmington.casino.games.coinflip;
 import java.util.Random;
 
 public class FlipCoinGame {
-    int heads = 1;
+    int heads = 2;
     int tails = 1;
     static Random random = new Random();
+
+    //        System.out.println("Howdy! Thanks for playing Coin Flip. If you have a gambling addiction please call 1-800-GAMBLER");
+//        System.out.println("How Much would you like to bet?");
+//        System.out.println("Type 2 for Heads or 1 for Tails. Choose wisely...");
+
     public static int flipCoin(int heads, int tails) {
 
         int randNumber = random.nextInt(2)+1;
@@ -20,16 +25,28 @@ public class FlipCoinGame {
     }
 
 
-
-
-    public static void main(String[] args) {
-
-
-    }
-
     public boolean playerWinsOrLose() {
 
 
         return false;
     }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("\n" +
+                " _____  _  _          ____          _         \n" +
+                "|___  || |(_)  __ _  |___ \\   ___  (_)  __ _  \n" +
+                "   _| || || | / _` |     | | / _ \\ | | / _` | \n" +
+                "  |_  || || || (_| |  ___| || (_) || || | | | \n" +
+                "    |_||_||_| \\__, | |____/  \\___/ |_||_| |_| \n" +
+                "                 |_|                          \n");
+
+                System.out.println("Howdy! Thanks for playing Coin Flip. If you have a gambling addiction please call 1-800-GAMBLER");
+        System.out.println("How Much would you like to bet?");
+        System.out.println("Type 2 for Heads or 1 for Tails. Choose wisely...");
+
+    }
+
+
 }
--- src/test/java/com/github/zipcodewilmington/flipcointests/PlayerWinsorLoseTest.java
+++ src/test/java/com/github/zipcodewilmington/flipcointests/PlayerWinsorLoseTest.java
@@ -14,7 +14,6 @@ public class PlayerWinsorLoseTest {
     boolean actual = game.playerWinsOrLose();
 
 
-
     }
 
 
